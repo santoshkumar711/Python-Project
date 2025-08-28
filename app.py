c4edd618ce3e99807afa0e3e8df507bfeaa0b818
Santosh Kumar <santoshkumar711711@gmail.com>
2025-08-29 01:56:52 +0530

    flask add

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-print("Hello world from Docker deployment")
-print("check Docker after jenkins check ")
-input("Press Enter to exit...")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello world from Docker deployment! Check Docker after Jenkins check."
+
+if __name__ == "__main__":
+    # Container ko alive rakhne ke liye Flask server run karenge
+    app.run(host="0.0.0.0", port=5000)
